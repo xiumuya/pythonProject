7af202b988e5fe3d19860f81c9f553040914c0c1
shixiumuya <1129987303@qq.com>
2022-12-14 15:26:53 +0800

    python

---
+++ 用re屠戮盗版天堂.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+import  requests
+import re
+import urllib3
+from timeit import timeit
+
+urllib3.disable_warnings()
+url='https://www.dy2018.com/'
+resp=requests.get(url,verify=False)
+resp.encoding='gb2312'
+page_content=resp.text
+object=re.compile(r'<li><a href=\'(?P<net>.*?)\''
+                  r'.*?title="(?P<name>.*?)"',re.DOTALL)
+result=object.finditer(page_content)
+for it in result:
+    with open('some_movies.txt', 'a', encoding='utf-8') as f:
+        print(it.group('net'))
+        print(it.group('name'),file=f)
+        # f.write(pprint.pformat(it.groupdict().values()))
+        newurl= url +it.group('net')
+        resp1=requests.get(newurl,verify=False)
+        resp1.encoding='gb2312'
+        page_content1=resp1.text
+        object1=re.compile(r'<td style="WORD-WRAP: break-word" bgcolor="#fdfddf"><a href="(?P<xl>.*?)">',re.DOTALL)
+        result1=object1.finditer(page_content1)
+        for it1 in result1:
+            print(it1.group('xl'),file=f)
+        resp1.close()
+resp.close()
+print('over')
+
